4f98deed0314711690e7b2a8c146c863d7055374
hadas <hadasabu67@gmail.com>
2025-04-02 20:40:43 +0300

    jest change to compileSdk = 35 from compileSdk = 34 because the app didn't run

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.myapplication15325"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.myapplication15325"
